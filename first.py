d28a4c4bbaf82916a1d482c594ab8bbddf83c7e1
zloyvetal <zloyvetal@gmail.com>
2020-05-19 16:09:17 +0300

    new feature : read_from_id - create json's with info about id's
    minor fixes

--- first.py
+++ first.py
@@ -4,17 +4,28 @@
 """
 import urllib3
 import json
+import requests
 
 my_kay = "mnrOlvNOeTnNJHyFrkjk6RFZ0NfDkftYxlO2cD1t"
 
 
+def url_search(search_url: str) -> str:
+    # берет строку с функции take_search_string и добавляет ее в полную строку для запроса
+    search_line = f"https://developers.ria.com/auto/search?api_key=YOUR_API_KEY&{search_url}"
+    return search_line
+
+
+def take_search_string(url_from_site: str, key: str) -> str:
+    # берет поисковую строку с сайта и превращает ее в поисковую строку для апи
+    r = requests.get(f'https://developers.ria.com/new_to_old?api_key={key}&{url_from_site}')
+    data = json.loads(r.text)
+    return data['string']
+
+
 def create_json_with_data(input_url: str, api_key: str, json_name: str):
     """
-    This function for download data from API and save it in .json file.
-    :param input_url:
-    :param api_key:
-    :param json_name:
-    :return:
+    принимает готовую поисковую строку с функции - url_search и отправляет запрос к АП
+    после ответа - сохраняет данные в файл с названием json_name которое тоже передается в эту функцию
     """
     http = urllib3.PoolManager()
 
@@ -30,13 +41,13 @@ def create_json_with_data(input_url: str, api_key: str, json_name: str):
         json.dump(data_from_api, f)
 
 
-def url_search(search_url: str) -> str:
-    # create full search line
-    search_line = f"https://developers.ria.com/auto/search?api_key=YOUR_API_KEY&url_search={search_url}"
-    return search_line
-
-
 def find_id(json_name: str) -> list:
+    """
+    Принимается имя файла который должен быть уже создан на пк, с данными от АПИ и достаются от туда все айдишники
+    объявлений
+
+    """
+
     # open json with data
     all_search_data = json.load(open(f'{json_name}'))
 
@@ -63,9 +74,16 @@ def find_id(json_name: str) -> list:
 def read_from_id(list_with_ids: list):
     for ids in list_with_ids:
         create_json_with_data(f'https://developers.ria.com/auto/info?api_key=YOUR_API_KEY&auto_id={ids}', my_kay,
-                              f'{ids}.json')
+                              f'{ids}')
 
 
-lets_try = find_id('bmwX6.json')
+def main():
 
-read_from_id(lets_try)
+    create_json_with_data(
+        url_search(take_search_string('categories.main.id=1&indexName=auto&brand.id[0]=47&model.id[0]=393', my_kay)),
+        my_kay, 'Mazda6')
+    read_from_id(find_id('Mazda6.json'))
+
+
+if __name__ == main():
+    main()
